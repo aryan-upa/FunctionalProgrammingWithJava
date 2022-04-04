b176ad63de5b438879827efbf9e608956133ae11
Aryan Upadhyay <aryan.offc@gmail.com>
2022-04-05 02:40:09 +0530

    created _Consumer.java file and made a consumer method.

---
+++ src/FunctionalInterface/_Consumer.java
@@ -0,0 +1,40 @@
+package FunctionalInterface;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class _Consumer {
+    public static void main(String[] args) {
+        // greetCustomer(new Customer("Maria", "123456789"));
+
+        // Using Consumer
+        greetCustomerConsumer.accept(new Customer("Maria", "123456879"));
+
+    }
+
+    // This is the primitive method of a function taking an object as argument and using
+    // its fields to produce some output.
+    static void greetCustomer(Customer cust) {
+        System.out.println("Hello " + cust.cusName +
+                ", thanks for registering with Ph. No " + cust.cusPhoneNum + "\n");
+    }
+
+    static class Customer {
+        private final String cusName;
+        private final String cusPhoneNum;
+
+        public Customer(String cusName, String cusPhoneNum) {
+            this.cusName = cusName;
+            this.cusPhoneNum = cusPhoneNum;
+        }
+    }
+
+    // Using Functional Interface
+
+    // Consumer -> takes an argument but returns nothing.
+    static Consumer<Customer> greetCustomerConsumer = customer -> {
+        System.out.println("Hello " + customer.cusName
+        + ", thanks for sharing your Ph. No. with us " + customer.cusPhoneNum + "\n");
+    };
+    
+}
