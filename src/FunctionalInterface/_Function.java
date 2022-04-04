e95851da774a8969618ec38c9f5e89e3f9ee4917
Aryan Upadhyay <aryan.offc@gmail.com>
2022-04-05 02:00:54 +0530

    Chaining multiple functions together.

--- src/FunctionalInterface/_Function.java
+++ src/FunctionalInterface/_Function.java
@@ -6,12 +6,23 @@ public class _Function {
     public static void main(String[] args) {
         int incrementedValue = incrementByOneFunction.apply(1);
         System.out.println(incrementedValue);
+
+        int multipliedValue = multiplyBy10Function.apply(incrementedValue);
+        System.out.println(multipliedValue);
+
+        //Now If we want to chain multiple function we can do that using the andThen method.
+        Function<Integer, Integer> add1AndThenMultiplyBy10 = incrementByOneFunction.andThen(multiplyBy10Function);
+        System.out.println("andThen Output\n" + add1AndThenMultiplyBy10.apply(1));
+
     }
 
-    static Function<Integer, Integer> incrementByOneFunction = number -> number++;
+    static Function<Integer, Integer> incrementByOneFunction = number -> number+1;
     // Function<Input Type, Return Type> functionName = var -> changeInVariable;
     // This is basically a function defined as a variable in the same class in which we want to use it.
     // To give the value to the only parameter we use apply method.
     // These are functional Interface.
+
+    static Function<Integer, Integer> multiplyBy10Function = number -> number*10;
+
 
 }
